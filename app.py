09fa6fd60e81b160cd9c85715ea007517c179660
Adam <39442291+Aurathic@users.noreply.github.com>
2022-04-10 08:02:54 -0400

    Remove pandas dependency, minor tweaks

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 # app.py
-#from crypt import methods
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-import pandas as pd
+import csv 
 import sys
 
-df = pd.read_csv("notebook_dataset.csv")
+
+
+
+with open("notebook_dataset.csv", "r") as f:
+    reader = csv.DictReader(f)
+    df = list(reader)
+for row in df:
+    row['performance'] = float(row['performance'])
+    row['portability'] = float(row['portability'])
+    row['screen_qual'] = float(row['screen_qual'])
+    row['battery_qual'] = float(row['battery_qual'])
+    row['Price_euros'] = float(row['Price_euros'])
+
+print(df, file=sys.stderr)
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-#creates dictionary with results, make webpage from that.
 @app.route('/results', methods=['GET', 'POST'])
 def results():
     #print(request.form, file=sys.stderr)
@@ -17,16 +28,17 @@ def results():
     portability = float(request.form['portability'][:-1])/100
     screen_quality = float(request.form['screen_quality'][:-1])/100
 
-    df['score'] = df.apply(lambda row:
-        performance * row['performance'] + 
-        portability * row['portability'] + 
-        screen_quality * row['screen_qual']
-    , axis=1)
+    df_priced = list(filter(lambda row: price_min <= float(row['Price_euros']) or float(row['Price_euros']) <= price_max, df))
 
-    df_priced = df[df['Price_euros'].between(price_min, price_max)]
-    df_priced.sort_values(by="score", ascending=False, inplace=True)
-    print(df_priced.to_dict(orient="records"), file=sys.stderr)
-    return render_template('results.html', data=df_priced.to_dict(orient="records"))
+    df_priced = list(map(lambda row:
+        row | {"score": 
+            performance *row['performance'] + 
+            portability * row['portability'] + 
+            screen_quality * row['screen_qual']}, 
+        df_priced))
+
+    df_priced.sort(key=lambda row: row["score"], reverse=True)
+    return render_template('results.html', data=df_priced)
 
 #Index page.
 @app.route('/') 
