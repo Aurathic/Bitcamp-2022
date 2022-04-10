002f1fd921827ec13063f444e50676ff290d8971
Sharath Kannan <skannan1@terpmail.umd.edu>
2022-04-09 20:33:54 -0400

    turned index into a form

--- app.py
+++ app.py
@@ -1,55 +1,40 @@
 # app.py
-from flask import Flask, render_template, request, jsonify
+from crypt import methods
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
 # MAIN WEBPAGE
-@app.route('/')
+# API KEYS HERE. ADAM'S GOT THE ALGORITHM
+
+#creates dictionary with results, make webpage from that.
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    '''
+    data = {}
+    data["Price"] = request.form["price"]
+    data["Performance"] = request.form["performance"]
+    data["Portability"] = request.form["portability"]
+    data["Screen Quality"] = request.form["screenquality"]
+    '''
+    data = {}
+
+    return render_template('results.html')
+
+#Index page.
+@app.route('/') 
 def index():
     return render_template('index.html')
 
 
-@app.route('/results')
-def results():
-    return "<h1>LoL</h1>"
+@app.route('/getinfo', methods=['GET', 'POST'])
+def getinfo():
+    data = {}
+    #data["Price"] = request.form["price"]
+    data["Performance"] = request.form["performance"]
+    data["Portability"] = request.form["portability"]
+    data["Screen Quality"] = request.form["screenquality"]
+    return data
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
