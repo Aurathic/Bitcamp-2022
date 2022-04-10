870f1e3662a75d9aeb513c889c47855aaf8627d1
Adam <39442291+Aurathic@users.noreply.github.com>
2022-04-10 05:52:33 -0400

    Get initial data from web form, sort by values (?)

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 # app.py
 #from crypt import methods
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-import recommend
+import pandas as pd
+import sys
 
-
+df = pd.read_csv("notebook_dataset.csv")
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 #creates dictionary with results, make webpage from that.
 @app.route('/results', methods=['GET', 'POST'])
 def results():
-    
-    user_dict = {'prices' : request.args.get('ret_price'), 
-                'performance' : request.args.get('ret_prefo'), 
-                'portability' : request.args.get('ret_port'), 
-                'screen_quality' : request.args.get('ret_SQ')}
-    
+    print(request.form, file=sys.stderr)
+    price_min, price_max = list(map(int, request.form['price'].replace("$", "").split("-")))
+    performance = float(request.form['performance'][:-1])/100
+    portability = float(request.form['portability'][:-1])/100
+    screen_quality = float(request.form['screen_quality'][:-1])/100
 
-    return render_template('results.html')
+    df['score'] = df.apply(lambda row:
+        performance * row['performance'] + 
+        portability * row['portability'] + 
+        screen_quality * row['screen_qual']
+    , axis=1)
+
+    df.sort_values(by="score", ascending=False, inplace=True)
+
+    return render_template('results.html', data=df.to_dict())
 
 #Index page.
 @app.route('/') 
